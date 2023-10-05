828b2a049cbf299aa8680157cd115ec9e0afcfbd
Bhaskar Ruidas <122375092+Bhaskar879@users.noreply.github.com>
2023-10-05 23:01:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+window.onload = function () {
+    setTimeout(function () {
+        document.getElementById('preloder').style.display = "none";
+        document.getElementById('content').style.display = "block";
+    }, 900);
+};
+
