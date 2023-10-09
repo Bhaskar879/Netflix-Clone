5d9c133dd75139a1be905d585f887f6757614f02
Bhaskar Ruidas <122375092+Bhaskar879@users.noreply.github.com>
2023-10-09 22:08:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ window.onload = function () {
     setTimeout(function () {
         document.getElementById('preloder').style.display = "none";
         document.getElementById('content').style.display = "block";
+        AOS.init();
     }, 900);
 };
 
